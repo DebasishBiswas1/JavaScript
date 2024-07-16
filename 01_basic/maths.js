4b85f397e1458a4398585085054e312b9f3ecfec
Debasish Biswas <126973301+DebasishBiswas1@users.noreply.github.com>
2024-07-16 03:28:56 +0000

    Maths functions

---
+++ 01_basic/maths.js
@@ -0,0 +1,10 @@
+console.log(Math.random());
+
+console.log(Math.random()* 10 + 1);
+
+console.log(Math.floor(Math.random()* 10 + 1));
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
