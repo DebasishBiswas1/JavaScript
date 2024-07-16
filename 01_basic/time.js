0f4288649b4011d57c4f067781d2beefaa0453aa
Debasish Biswas <126973301+DebasishBiswas1@users.noreply.github.com>
2024-07-16 04:14:39 +0000

    date and time

---
+++ 01_basic/time.js
@@ -0,0 +1,11 @@
+console.log(Date());
+console.log(Date(0));
+
+let date = new Date();
+console.log(date.getFullYear());
+
+let todayDate1 = new Date(2024 , 1, 23)
+console.log(todayDate1.toDateString());
+
+let todayDate2 = new Date(2024 , 1, 23, 5, 6)
+console.log(todayDate2.toLocaleString());
