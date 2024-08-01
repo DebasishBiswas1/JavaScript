647de354595bc1fe60cd4cd29e07e8cd8c55b295
Debasish Biswas <126973301+DebasishBiswas1@users.noreply.github.com>
2024-08-01 06:51:52 +0000

    added changes to map

--- 01_basic/map.js
+++ 01_basic/map.js
@@ -7,9 +7,41 @@ map.set("FR","France")
 map.set("UK","United Kingdom")
 
 for (const key of map) {
-    console.log(key);
+    // console.log(key);
 }
 
 for (const [key,value] of map) {
-    console.log(key+" :- "+value);
+    // console.log(key+" :- "+value);
 }
+
+
+const coding = ["js","cpp","c","py","ruby"]
+
+coding.forEach( (item)=>{
+    // console.log(`Coding short form: ${item}`);
+})
+
+function printMe( item ){
+    console.log(item);
+}
+
+// coding.forEach(printMe)
+
+coding.forEach( (item,index,arr)=>{
+    // console.log(item,index,arr);
+})
+
+const codingLang = [
+    {
+        language:"javascript",
+        shortForm:"js"
+    },
+    {
+        language:"java",
+        shortForm:"js"
+    },
+    {
+        language:"javascript",
+        shortForm:"js"
+    }
+]
