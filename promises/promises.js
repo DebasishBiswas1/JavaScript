ab5e6b82ef744de8d7f6bee07bd8770e2db8e431
Debasish Biswas <126973301+DebasishBiswas1@users.noreply.github.com>
2024-08-28 06:09:50 +0000

    promise_two,three,four

--- promises/promises.js
+++ promises/promises.js
@@ -24,3 +24,39 @@ new Promise(function(resolve,reject){
     console.log("promise 2 cunsumed");
     
 })
+
+// promise three
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"deba",email:"deba@example.com"});
+        
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+// promise four
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        const error = false;
+        if(!error){
+            resolve({username:"debasish",pasword:"123"});
+        }else{
+            reject('ERROR: Something went wrong');
+        }
+        
+    },1000)
+})
+
+promiseFour.then(function(user){
+    console.log(user);
+    return user.username;
+}).then( (username)=>{
+    console.log(username);
+}).catch( (error)=>{
+    console.log(error);
+})
